4e0fde0da969a6b642e202c6f28118acebca6fca
ChhunKuySreng <chhunkuy@gmail.com>
2024-04-05 10:55:07 +0700

    Route Name

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,16 +3,17 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom
 import BasePage from "./views";
 import Home from "./views/web/home";
 import Menu from "./views/web/menu";
+import {APP_NAME, IS_APP_PRODUCTION} from "./constants/app";
 
 export default function routes() {
   return (
-    <Router>
+    <Router basename={IS_APP_PRODUCTION ? "" : APP_NAME}>
       <Routes>
         <Route path="/" element={<BasePage />}>
           <Route path="/home" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
         </Route>
-        <Route path="/*" element={<Navigate to="/home" />}/>
+        <Route path="/*" element={<Navigate to="/home" />} />
       </Routes>
     </Router>
   );
