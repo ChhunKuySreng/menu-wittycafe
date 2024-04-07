e9e1ac7010e7bc4af9798e4dbf69ae5434f34ab7
ChhunKuySreng <chhunkuy@gmail.com>
2024-04-07 23:21:43 +0700

    Update List Drink Menu add Menu Image

--- src/components/sliderCarousel.jsx
+++ src/components/sliderCarousel.jsx
@@ -1,26 +1,48 @@
+import React, {useEffect, useState} from "react";
+import {Box, Flex, IconButton, Image, Modal, ModalContent, ModalOverlay, ModalCloseButton, ModalBody, ModalFooter, ModalHeader, useDisclosure} from "@chakra-ui/react";
 import {ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
-import {Box, Flex, IconButton} from "@chakra-ui/react";
-import React, {useState} from "react";
+import {slide} from "../constants/variables";
+import {toAbsoluteUrl} from "../utils/helper";
 
-export default function SliderCarousel({slides}) {
+export default function SliderCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const {isOpen, onOpen, onClose} = useDisclosure();
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === slides.length - 1 ? 0 : prevIndex + 1));
-  };
+  useEffect(() => {
+    const interval = !isOpen && setInterval(nextSlide, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex, isOpen]);
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? slides.length - 1 : prevIndex - 1));
-  };
+  const nextSlide = () => setCurrentIndex((prevIndex) => (prevIndex === slide.length - 1 ? 0 : prevIndex + 1));
+  const prevSlide = () => setCurrentIndex((prevIndex) => (prevIndex === 0 ? slide.length - 1 : prevIndex - 1));
+  const openModal = () => onOpen();
+  const closeModal = () => onClose();
+
   return (
     <Flex justify="center" align="center" w="100%" h="100%" position="relative">
-      <IconButton icon={<ChevronLeftIcon />} color={"white"} variant={"outline"} onClick={prevSlide} position="absolute" left="1rem" zIndex="1" />
-      {slides.map((slide, index) => (
-        <Box key={index} w="100%" h="100%" opacity={index === currentIndex ? 1 : 0} transition="opacity 0.5s ease" position="absolute" left={0} top={0}>
-          {slide}
+      <IconButton icon={<ChevronLeftIcon />} color="#F4B223" variant="outline" onClick={prevSlide} position="absolute" left="1rem" zIndex="1" />
+      {slide.map((slides, index) => (
+        <Box key={index} w="100%" h="100%" opacity={index === currentIndex ? 1 : 0} transition="opacity 0.5s ease" position="absolute" left={0} top={0} onClick={openModal}>
+          <Image src={toAbsoluteUrl(`/assets/${slides.src}`)} pos="absolute" top={{base: "-40px", md: "-130px"}} alt={slides.src} />
         </Box>
       ))}
-      <IconButton icon={<ChevronRightIcon />} color={"white"} variant={"outline"} onClick={nextSlide} position="absolute" right="1rem" zIndex="1" />
+      <IconButton icon={<ChevronRightIcon />} color="#F4B223" variant="outline" onClick={nextSlide} position="absolute" right="1rem" zIndex="1" />
+      <Modal isCentered isOpen={isOpen} onClose={closeModal} size="xl" motionPreset="slideInBottom">
+        <ModalOverlay backdropFilter="blur(10px)" />
+        <ModalContent bg="#633510" color="white">
+          <ModalHeader>Image Preview</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <a href={toAbsoluteUrl(`/assets/${slide[currentIndex].src}`)} target="_blank" rel="noopener noreferrer">
+              <Image src={toAbsoluteUrl(`/assets/${slide[currentIndex].src}`)} alt={slide[currentIndex].src} />
+            </a>
+          </ModalBody>
+          <ModalFooter>
+            <IconButton icon={<ChevronLeftIcon />} color="#F4B223" variant="outline" onClick={prevSlide} mr={3} />
+            <IconButton icon={<ChevronRightIcon />} color="#F4B223" variant="outline" onClick={nextSlide} />
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
--- src/views/web/home/index.jsx
+++ src/views/web/home/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import {Box, Divider, Flex, Image, Spacer, Text} from "@chakra-ui/react";
+import {Box, Divider, Flex, Spacer, Text} from "@chakra-ui/react";
 import SliderCarousel from "../../../components/sliderCarousel";
-import {toAbsoluteUrl} from "../../../utils/helper";
 import {menulist} from "../../../constants/variables";
 import Footer from "../../../components/footer";
 
 export default function Home({searchQuery}) {
-  const slides = [
-    <Box key="slide1" w="100%" h="100%">
-      <Image src={toAbsoluteUrl("/assets/1.jpg")} pos="absolute" top={{base: "-40px", md: "-130px"}} />
-    </Box>,
-    <Box key="slide2" w="100%" h="100%" bg="green" />,
-    <Box key="slide3" w="100%" h="100%" bg="blue" />,
-  ];
-
-  // Filter menu items based on search query
   const filteredMenuItems = menulist.reduce((acc, menuList) => {
     const filteredItems = menuList.items.filter((item) => item.name.toLowerCase().includes(searchQuery.toLowerCase()));
     if (filteredItems.length > 0) {
@@ -26,20 +16,16 @@ export default function Home({searchQuery}) {
   return (
     <>
       <Box px={4} py={6} color={"white"}>
-        {/* Conditionally render SliderCarousel based on presence of searchQuery */}
         {searchQuery === "" && (
           <Box w="100%" h="300px" overflow="hidden" rounded={20} boxShadow="dark-lg">
-            <SliderCarousel slides={slides} />
+            <SliderCarousel />
           </Box>
         )}
-        {/* Render filtered menu items or 'can't find item' text */}
         {filteredMenuItems.length === 0 ? (
-          // Render 'can't find item' text when no items are found
           <Text fontSize="xl" fontWeight="bold" textAlign="center">
             Can't find item
           </Text>
         ) : (
-          // Render filtered menu items
           filteredMenuItems.map((menuList) => (
             <Box key={menuList.id} id={menuList.id}>
               <Text fontSize="2xl" fontWeight="bold" pt={10}>
